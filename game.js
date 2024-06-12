f81472b21c249428f9f2a4bd5ddf56c3be8c04de
naoualloli <naoualloli967@gmail.com>
2024-06-12 10:42:34 +0200

    Edit and add

--- game.js
+++ game.js
@@ -14,15 +14,7 @@ var gallery=document.getElementById("gallery");
     }
 
 
-    let nowimg = document.querySelector('.nowImg');
-
-    function clickImg(classe){
-        nowimg.src = classe
-    }
     
-    function clickImg1(classe){
-        nowimg.src = classe
-    }
 
 
 

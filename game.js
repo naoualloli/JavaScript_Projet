a7dc4837ecc26aa64d43742ba8d264167bccf99a
naoualloli <naoualloli967@gmail.com>
2024-06-12 19:52:09 +0200

    Edit and add

--- game.js
+++ game.js
@@ -14,6 +14,7 @@ var gallery=document.getElementById("gallery");
     }
 
 
+ 
     
 
 
@@ -50,3 +51,5 @@ var gallery=document.getElementById("gallery");
     signup_link.style.background = "#2981bc";
     login_link.style.background = "#34495e";
   }
+
+  
